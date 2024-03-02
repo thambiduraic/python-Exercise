3cf3144d2aed9baed8876321d22752e1a5ec9af4
Thambidurai Chennaiyen <74646258+thambiduraic@users.noreply.github.com>
2024-03-02 17:51:13 +0530

    Mail send using python
    Send a mail with html file to some one.

---
+++ Mail/mail_with_html.py
@@ -0,0 +1,44 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+
+def send_email(sender_email, sender_password, receiver_email, subject, html_file_path):
+    # Create a multipart message
+    message = MIMEMultipart()
+
+    # Setup the parameters of the message
+    message['From'] = sender_email
+    message['To'] = receiver_email
+    message['Subject'] = subject
+
+    # Read the HTML file
+    with open(html_file_path, 'r') as html_file:
+        html_content = html_file.read()
+
+    # Attach HTML content
+    html_part = MIMEText(html_content, 'html')
+    message.attach(html_part)
+
+    # Create SMTP session
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    
+    # Login to the SMTP server
+    server.login(sender_email, sender_password)
+
+    # Send email
+    server.send_message(message)
+
+    # Quit the SMTP server
+    server.quit()
+
+# Example usage
+sender_email = 'suginkl@gmail.com'
+sender_password = 'gqklgxwicjjijwnf'
+receiver_email = 'suginkl@gmail.com'
+subject = 'HTML File Test'
+html_file_path = 'D:\Thambidurai\pycharm_work_dir\PycharmProjects\pythonProject\Mail\demo.templatemonster.com-1709368894\index.html' # add your html file path
+html_file_path.replace("\\", "/")
+
+send_email(sender_email, sender_password, receiver_email, subject, html_file_path)
